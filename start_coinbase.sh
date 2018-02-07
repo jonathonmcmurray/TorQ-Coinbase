4edbe97ad673fe9a643c60032f76edadca88a364
jonathonmcmurray <jonathon.mcmurray@aquaq.co.uk>
2018-02-07 20:03:14 +0000

    move fields into extras col in config

--- start_coinbase.sh
+++ start_coinbase.sh
@@ -10,10 +10,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KDBLIB/l32
 getfield() {
   fieldno=`awk -F, '{if(NR==1) for(i=1;i<=NF;i++){if($i=="'$2'") print i}}' appconfig/process.csv`  # get number for field based on headers
   fieldval=`awk -F, '{if(NR == '$1') print $'$fieldno'}' appconfig/process.csv`                     # pull one field from one line of file
+  echo $fieldval | envsubst                                                                       # substitute env vars
+ }
+
+parameter() {
+  fieldval=`getfield $1 $2`
   if [ "" == "$fieldval" ]; then                                                                    # check for empty string
-    echo ""                                                                                         # return nothing if nothing in config
+    echo ""
   else
-    echo " -"$2 $fieldval | envsubst                                                                # append comand line param at start of value, substitute env vars
+    echo " -"$2 $fieldval
   fi
  }
 
@@ -24,15 +29,16 @@ findproc() {
 start() {
   if [ -z `findproc $1` ]; then
     procno=`awk '/'$1'/{print NR}' appconfig/process.csv`                                           # get line number for file
-    params="proctype U localtime g T w load schemafile tplogdir"                                    # list of params to read from config
+    params="proctype U localtime g T w load"                                                        # list of params to read from config
     sline="q ${TORQHOME}/torq.q -procname $1 ${KDBSTACKID}"                                         # base part of startup line
     for p in $params;                                                                               # iterate over params
     do
-      a=`getfield $procno $p`;                                                                      # get param
+      a=`parameter $procno $p`;                                                                     # get param
       sline="$sline$a";                                                                             # append to startup line
     done
+    sline="$sline $(getfield $procno extras)"
     echo "Starting $1..."
-    eval "${sline} </dev/null >${KDBLOG}/torq${1}.txt 2>&1 &"                                       # redirect output and run in background
+    echo "${sline} </dev/null >${KDBLOG}/torq${1}.txt 2>&1 &"                                       # redirect output and run in background
   else
     echo "$1 already running"
   fi
@@ -45,7 +51,7 @@ getall() {
   do
     procno=`awk '/'$a'/{print NR}' appconfig/process.csv`                                           # get line number for file
     f=`getfield $procno startwithall`
-    if [ " -startwithall 1" == "$f" ]; then
+    if [ "1" == "$f" ]; then
       start="$start $a"
     fi
   done
